0968b8e1cdd59768bf0028f11c57c3c22034e8eb
CI <ci@priware.ir>
2024-10-28 19:47:03 +0330

    Deploy website - based on 6d94dec4f79aec3826f6fbbf3863e44cc5d7f918

---
+++ assets/js/1a1424c7.51f1383f.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkalishir_github_io=self.webpackChunkalishir_github_io||[]).push([[434],{6119:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/","page":1,"postsPerPage":10,"totalPages":1,"totalCount":3,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
